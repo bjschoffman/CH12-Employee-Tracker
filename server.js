0eb924b98d26812889546cefbf3c41d63f58b06b
bjschoffman <bjschoffman@gmail.com>
2023-02-23 20:16:38 -0600

    add updates

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+require('dotenv').config();
 require('console.table');
 const inquirer = require ('inquirer');
-const db = require('./connection');
+const mysql = require('mysql2');
 
 
-db.connect(err => {
+const connection = mysql.createConnection (
+    {
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: process.env.DB_PASSWORD,
+      database: 'employee_db',
+    });
+
+connection.connect((err) => {
     if (err) throw err;
     console.log(`Connected to the employee_db database.`);
     employee_db();
@@ -13,7 +23,7 @@ const questions = () => {
     inquirer
     .prompt ([{
         type: 'list',
-        name: 'options',
+        name: 'choices',
         message: 'What would like to do?',
         choices: [
             'View All Departments',
@@ -26,23 +36,84 @@ const questions = () => {
             'Exit'
         ],
 }])
-.then((answer) => {
-if(answer.options === 'View All Departments'){
-viewAllDepartments()}
-});
-}
-function viewAllDepartments (){
-db.promise().query('SELECT * FROM department;')
-.then(([rows])=>{
-    let departments = rows;
-    console.log('\n');
-    console.table(departments)
+    .then((answer) => {
+    if (answer.choices === "View All Departments") {
+        viewAllDepartments();
+    }
 
-}) .then(()=>{
-    questions();
-})
-}
+    if (answer.choices === "View All Roles") {
+        viewAllRoles();
+    }
+
+    if (answer.choices === "View All Employees") {
+        viewAllEmployees();
+    }
+
+    if (answer.choices === "Add A Department") {
+        addDepartment();
+    }
+
+    if (answer.choices === "Add A Role") {
+        addRole();
+    }
+
+    if (answer.choices === "Add An Employee") {
+        addEmployee();
+    }
+
+    if (answer.choices === "Update An Employee Role") {
+        updateEmployeeRole();  
+    }
+
+    if (answer.choices === "Exit") {
+        exit();
+    }
+
+    });
+};
+
+function viewAllDepartments() {
+    db.promise().query('SELECT * FROM departments')
+    .then(([rows])=>{
+        let departments = rows;
+        console.log('\n');
+        console.table(departments)
+    }) .then(()=>{
+        questions();
+    });
+};
+
+function viewAllRoles () {
+    db.promise().query('SELECT * FROM role')
+    .then(([rows])=>{
+        let departments = rows;
+        console.log('\n');
+        console.table(departments)
+    }) .then(()=>{
+        questions();
+    });
+};   
+
+function viewAllEmployees () {
+    db.promise().query('SELECT * FROM employee')
+    .then(([rows])=>{
+        let departments = rows;
+        console.log('\n');
+        console.table(departments)
+    }) .then(()=>{
+        questions();
+    });
+};
+
+
+
+
+
 questions();
+
+
+
+
 
 
 
